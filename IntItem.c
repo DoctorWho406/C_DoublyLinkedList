43a91dcc2cb7eecf0ceacc48016c293c4479e003
Lorenzo Guerrini <lollo.guerrini@gmail.com>
2022-11-22 22:06:17 +0100

    Add IntItem create function

---
+++ IntItem.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "Functions.h"
+
+struct int_item *int_item_new(const int value) {
+    struct int_item *item = malloc(sizeof(struct int_item));
+    if (!item) {
+        return NULL;
+    }
+    item->value = value;
+    return item;
+}
